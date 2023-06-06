acc7a313a0d2e5d6eeee656fca9da8bb1bb8684b
Jack Wei <jack_weq@icloud.com>
2023-06-05 22:55:03 -0400

    working first template

--- src/main/java/frc/robot/subsystems/Motor.java
+++ src/main/java/frc/robot/subsystems/Motor.java
@@ -4,55 +4,26 @@
 
 package frc.robot.subsystems;
 
-import com.revrobotics.CANPIDController;
 import com.revrobotics.CANSparkMax;
-import com.revrobotics.RelativeEncoder;
-import com.revrobotics.SparkMaxPIDController;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
-import edu.wpi.first.math.controller.PIDController;
-import edu.wpi.first.math.system.plant.DCMotor;
-import edu.wpi.first.math.system.plant.LinearSystemId;
-import edu.wpi.first.wpilibj.Encoder;
-import edu.wpi.first.wpilibj.RobotController;
-import edu.wpi.first.wpilibj.simulation.EncoderSim;
-import edu.wpi.first.wpilibj.simulation.FlywheelSim;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.Constants;
-import frc.robot.Constants.ModuleConstants;
+import frc.robot.simWrapper.motors.CANSparkMaxWrapper;
 
 public class Motor extends SubsystemBase {
 	CANSparkMax m_motor;
-	Encoder m_encoder;
-	EncoderSim m_encoderSim;
-
-	PIDController m_PIDController = new PIDController(ModuleConstants.kPModuleDriveController, 0, 0);
-	
-	double m_driveOutput;
-
-	FlywheelSim m_driveMotorSim = new FlywheelSim(
-		LinearSystemId.identifyVelocitySystem(Constants.DriveConstants.kvVoltSecondsPerMeter, Constants.DriveConstants.kaVoltSecondsSquaredPerMeter),
-		ModuleConstants.kDriveMotorGearbox,
-		ModuleConstants.kDriveGearRatio
-	);
+	CANSparkMaxWrapper m_simWrapper;
 
 	/** Creates a new Motor. */
 	public Motor() {
 		m_motor = new CANSparkMax(0, MotorType.kBrushless);
-		m_encoder = new Encoder(0, 1);
-		m_encoderSim = new EncoderSim(m_encoder);		    
-
-		// m_encoder.setDistancePerPulse(ModuleConstants.kDriveEncoderDistancePerPulse);
-
-		// m_encoder.setReverseDirection(driveEncoderReversed);
-
+		m_simWrapper = new CANSparkMaxWrapper(m_motor, false);
 	}
 
-	public void set(double speedMetersPerSecond){
-		m_driveOutput = m_PIDController.calculate(m_encoder.getRate(), speedMetersPerSecond);
-		m_motor.set(m_driveOutput);
-		m_driveMotorSim.setInputVoltage(m_driveOutput / Constants.DriveConstants.kMaxSpeedMetersPerSecond * RobotController.getBatteryVoltage());
+
+	public void set(double d){
+		// this is also going to set the speed of acutal motor
+		m_simWrapper.set(d);
 	}
 
 	@Override
@@ -62,12 +33,6 @@ public class Motor extends SubsystemBase {
 
 	@Override
 	public void simulationPeriodic(){
-		m_driveMotorSim.update(0.2);
-		m_encoderSim.setRate(m_driveMotorSim.getAngularVelocityRadPerSec());
-		SmartDashboard.putNumber("sim rate", m_encoderSim.getRate());
-		SmartDashboard.putNumber("rate", m_encoder.getRate());
-		SmartDashboard.putNumber("output", m_driveOutput);
-		SmartDashboard.putNumber("sim angular vel", m_driveMotorSim.getAngularVelocityRPM());
-		SmartDashboard.putNumber("sim current draw amps", m_driveMotorSim.getCurrentDrawAmps());
+		m_simWrapper.simulationPeriodic();
 	}
 }
